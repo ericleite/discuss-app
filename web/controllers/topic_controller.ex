cb2b887712609d5738ecbcaa51f4b89d009895ee
Eric Leite <ericxleite@gmail.com>
2020-11-01 19:16:45 -0800

    added topic controller and model

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,15 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  def new(conn, params) do
+    struct = %Topic{}
+    params = %{}
+    changeset = Topic.changeset(struct, params)
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic" => topic}) do
+  end
+end
