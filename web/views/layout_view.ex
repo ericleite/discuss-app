f5de3c68360e3cc3613fe3fcf9c065a8281907d4
Eric Leite <ericxleite@gmail.com>
2020-11-07 15:41:31 -0800

    add active class method

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,15 @@
 defmodule Discuss.LayoutView do
   use Discuss.Web, :view
+
+  @doc """
+  Sets active class if path is currently active
+  """
+  @spec get_active_class(Plug.Conn.t(), String.t(), String.t(), String.t()) :: String.t()
+  def get_active_class(conn, path, defaultClass, activeClass) do
+    if path == conn.request_path do
+      defaultClass <> " " <> activeClass
+    else
+      defaultClass
+    end
+  end
 end
